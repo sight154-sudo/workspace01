1c2e4307061037a89725216991661f93decb5092
sight154 <sight154@gmail.com>
2022-03-30 00:43:47 +0800

    顺序队列

--- algorithm-practice/src/main/java/com/huawei/queue/ArrayQueue.java
+++ algorithm-practice/src/main/java/com/huawei/queue/ArrayQueue.java
@@ -7,13 +7,70 @@ package com.huawei.queue;
  */
 public class ArrayQueue {
     // 初始化大小
-    private String[] item;
+    private String[] items;
     private int n;
     private int head;
     private int tail;
 
-    public ArrayQueue(){
-        item = new String[10];
+    // 申请一个大小容量为capacity的队列
+    public ArrayQueue(int capacity) {
+        items = new String[capacity];
+        // 设置初始容量
+        n = capacity;
+    }
 
+    /**
+     * 入队   在队尾插入元素  在队首删除元素
+     *
+     * @param item
+     * @return
+     */
+    public boolean enqueue(String item) {
+        // 若n==tail,则表示元素已满，不能再放入元素
+        if (n == tail) {
+            if (head == 0) {
+                return false;
+            }
+            // 当容器中没有空闲空间时，触发一次元素的迁移
+            int len = tail - head;
+            for (int i = head; i < len; i++) {
+                items[i - head] = items[head + i];
+            }
+            head -= len;
+            tail -= len;
+        }
+        items[tail] = item;
+        tail++;
+        return true;
+    }
+
+    /**
+     * 出队
+     *
+     * @return
+     */
+    public String dequeue() {
+        if (head == tail) {
+            // 表示队列中已无元素
+            return null;
+        }
+        String item = items[head];
+        head++;
+        return item;
+    }
+
+
+    public static void main(String[] args) {
+        ArrayQueue queue = new ArrayQueue(6);
+        queue.enqueue("1");
+        queue.enqueue("2");
+        queue.enqueue("3");
+        queue.enqueue("4");
+        queue.dequeue();
+        queue.dequeue();
+        queue.enqueue("5");
+        queue.enqueue("6");
+        queue.enqueue("7");
+        queue.enqueue("8");
     }
 }
